56e2cddf3fce42633107fbfd2c1a7fad86a7d2d5
Eunicegeorge <georgeunice408@gmail.com>
2023-07-26 11:48:09 -0700

    more functions

--- shell.h
+++ shell.h
@@ -1,10 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define BUFSIZE 1024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <sys/types.h>
+
+void *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+void _memset(char *str, int fill, int n);
+void _memcpy(char *dest, char *src, unsigned int bytes);
+char *_getline(int file);
 
 void handle_status(char *file_name);
 void exce_cmd (char *args, char *file_name);
